8f0d31a96700d8bba01c2eb0108b6f6e00432f55
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-06-05 19:46:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store, persistor } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/tweetsApp">
+      <BrowserRouter basename="/tweetsApp/home">
         <React.StrictMode>
           <App />
         </React.StrictMode>
