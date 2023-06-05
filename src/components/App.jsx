9e948e5af78e700e9ca2fd1f8ea3631b61a048c3
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-06-05 19:43:49 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
-import HomePage from 'pages/HomePage';
-import TweetsPage from 'pages/TweetsPage/TweetsPage';
+import HomePage from '../pages/HomePage';
+import TweetsPage from '../pages/TweetsPage';
 
 export const App = () => {
   return (
