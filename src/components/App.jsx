70011a94c0302832077b1fc917d2c5320ab1e69c
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-06-05 19:55:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import HomePage from '../pages/HomePage';
 import TweetsPage from '../pages/TweetsPage';
@@ -7,8 +7,8 @@ import TweetsPage from '../pages/TweetsPage';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<SharedLayout />}>
+        {/* <Route path="/" element={<Navigate to="/home" replace />} /> */}
         <Route path="/home" element={<HomePage />} />
         <Route path="/tweets" element={<TweetsPage />} />
         <Route path="*" element={<HomePage />} />
