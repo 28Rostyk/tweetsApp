c715fc21728bec7f668f33f84b3d57ece760eb7c
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-06-08 15:57:08 +0300

    fixed routers

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import LogoIcon from '../../icon/LogoIcon';
 import { NavLink } from 'react-router-dom';
 
 const Logo = () => {
-  return <NavLink to="/">{LogoIcon}</NavLink>;
+  return <NavLink to="/home">{LogoIcon}</NavLink>;
 };
 
 export default Logo;
